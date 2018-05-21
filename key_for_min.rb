7996832e704e60a864e42572db8ac2c1b4376e57
Josh Stegmeier <josh@steg.me>
2018-05-21 23:09:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,8 @@
 
 def key_for_min_value(name_hash)
   name_hash.collect do |name, value|
+    bestVal = false
+    bestName = false
     if bestVal == false
       bestVal = value
       bestName = name
